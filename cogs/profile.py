d8e5cd9b21c86b7353b462ecbbc3920f98d15890
Dok#4440 <>
2021-05-07 21:02:53 +0000

    Push?

--- cogs/profile.py
+++ cogs/profile.py
@@ -49,6 +49,7 @@ class profile(commands.Cog):
           maleFemaleRatio = [1, 2]
           female_height = ["4'7", "4'8", "4'9", "4'10", "4'11", "5'", "5'1", "5'2", "5'3", "5'4", "5'5", "5'6", "5'7", "5'8"]
           male_height = ["4'9", "4'10", "4'11", "5'", "5'11", "6'", "6'1", "5'4", "5'5", "5'6", "5'7", "5'8", "5'9", "5'10"]
+          age = ['18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28']
           choice = random.choice(maleFemaleRatio)
 
           if choice == 1:
@@ -57,24 +58,26 @@ class profile(commands.Cog):
             first_name = random.choice(CharacterFirstNameMale)
             last_name = random.choice(CharacterLastName)
             height = random.choice(male_height)
+            age = random.choice(age)
           else:
             gender = "Female"
             looks = random.choice(FemaleLooks)
             first_name = random.choice(CharacterFirstNameFemale)
             last_name = random.choice(CharacterLastName)
             height = random.choice(female_height)
+            age = random.choice(age)
 
 
-          em=discord.Embed(color = 0xadcca6)
-          em.set_author(name=f"{first_name} {last_name}", icon_url="https://media.discordapp.net/attachments/804705780557021214/804827293087563776/Throwing_Knives.png")
-          em.add_field(name="Info Card", value=f"Gender: {gender}\nHeight: {height}\n Friend ID: 1234", inline=False)
+          em=discord.Embed(title=f"{first_name} {last_name}", color = 0xadcca6)
+          em.add_field(name="Info Card", value=f"Gender: {gender}\nHeight: {height}\nAge: {age}\n Friend ID: 1234", inline=False)
           em.add_field(name="Region", value="World: Heimur\nDistrict: Svart", inline=False)
           em.add_field(name="Level", value="Player Level: `0`\nPrimary Weapon: `N/A`\nSecondary Weapon: `N/A`", inline=False)
-          # em.set_footer(text="To add a friend, wait u impatient fuck.")
           em.set_thumbnail(url=looks)
           await ctx.send(embed=em)
 
+          user_name = f"{ctx.author.name}#{ctx.author.discriminator}"
 
+          collection.update_one({"_id": ctx.message.author.id}, {"$set":{"gender": "Male", "looks": looks, "first_name": first_name, "last_name": last_name, "height": height, "world": "Heimur", "district": "Svart", "friend_id": user_name, "age": age}}, upsert=True)
 
         else:
           await ctx.send(embed=discord.Embed(color=0xadcca6, description=f"**{ctx.author.name}#{ctx.author.discriminator}** The command was canceled."))
