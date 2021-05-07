bbf53c419ccb97a0e639e67e7a7c3e65f4adcda8
Dok#4440 <>
2021-05-07 11:29:35 +0000

    yet another try. 4

--- cogs/admin.py
+++ cogs/admin.py
@@ -75,7 +75,7 @@ class Admin(commands.Cog):
         else:
           em.description = f"**{ctx.author.name}#{ctx.author.discriminator}** Updating Project Ax.."
           msg = await ctx.send(embed = em)
-          msg = await ctx.fetch_message(msg.id)
+          # msg = await ctx.fetch_message(msg.id)
           collection.update_one({"_id": 1}, {"$set":{"channel_id": ctx.message.channel.id, "message_id": msg}}, upsert=True)
       else:
         if (os.system("sudo sh rAIOm.sh") / 256) > 1:
@@ -84,7 +84,7 @@ class Admin(commands.Cog):
         else:
           em.description = f"**{ctx.author.name}#{ctx.author.discriminator}** Shutting Down.."
           msg = await ctx.send(embed = em)
-          msg = await ctx.fetch_message(msg.id)
+          # msg = await ctx.fetch_message(msg.id)
           collection.update_one({"_id": 1}, {"$set":{"channel_id": ctx.message.channel.id, "message_id": msg}}, upsert=True)
 
     @die.error
